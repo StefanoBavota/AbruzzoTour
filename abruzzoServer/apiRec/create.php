2722abc514b2a29625ba5bf7e597a254d5477ce0
LucaEvangelista <luca10evangelista@gmail.com>
2021-06-23 13:05:32 +0200

    inizio rec

---
+++ abruzzoServer/apiRec/create.php
@@ -0,0 +1,25 @@
+<?php
+
+include "../config.php";
+$input = file_get_contents('php://input');
+$data = json_decode($input, true);
+$ut = 
+$message = array();
+$titolo = $data['titolo'];
+$descrizione = $data['descrizione'];
+$valutazione = $data['valutazione'];
+$id_utente = $
+
+
+$q = mysqli_query($con, "INSERT INTO recensioni (titolo, descrizione, valutazione, id_utente) VALUES ('$titolo', '$descrizione','$valutazione', '$id_utente') ");
+
+if($q){
+    http_response_code(201);
+    $message['status'] = "Success";
+} else {
+    http_response_code(422);
+    $message['status'] = "Error";
+}
+
+echo json_encode($message);
+echo mysqli_error($con);
