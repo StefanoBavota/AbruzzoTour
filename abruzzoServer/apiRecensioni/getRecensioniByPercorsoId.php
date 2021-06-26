fb05f0626ce4c5f640edfe9d1915254cbde85516
StefanoBavota <stefano.bavota@hotmail.com>
2021-06-26 18:37:53 +0200

    bug fix recensioni and login

--- abruzzoServer/apiRecensioni/getRecensioniByPercorsoId.php
+++ abruzzoServer/apiRecensioni/getRecensioniByPercorsoId.php
@@ -4,7 +4,7 @@ include "../config.php";
 $data = array();
 $id_percorso = $_GET['id'];
 
-$q = mysqli_query($con, "SELECT * FROM recensioni WHERE id_percorso = $id_percorso");
+$q = mysqli_query($con, "SELECT * FROM recensioni INNER JOIN utenti ON utenti.id = recensioni.id_utente WHERE id_percorso = $id_percorso");
 
 while ($row = mysqli_fetch_object($q)){
     $data[] = $row;
